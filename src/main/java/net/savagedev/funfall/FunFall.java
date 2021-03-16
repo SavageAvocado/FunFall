3dbf3473a10a9677d4be4006c664f8de5772fd47
SavageAvocado <acevedo72799@gmail.com>
2021-03-15 23:10:51 -0400

    Initial commit.

---
+++ src/main/java/net/savagedev/funfall/FunFall.java
@@ -0,0 +1,13 @@
+package net.savagedev.funfall;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class FunFall extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
